9301b9f48110b2916e6210a7aa84a100b5479d68
Lingcao Huang <huanglingcao@gmail.com>
2017-05-15 20:37:09 +0800

    add pytorch_deeplab_resnet codes

---
+++ pytorch_deeplab_resnet/README.md
@@ -0,0 +1,55 @@
+# pytorch-deeplab-resnet
+[DeepLab resnet](https://arxiv.org/abs/1606.00915) model implementation in pytorch. 
+
+The architecture of deepLab-ResNet has been replicated exactly as it is from the caffe implementation. This architecture calculates losses on input images over multiple scales ( 1x, 0.75x, 0.5x ). Losses are calculated individually over these 3 scales. In addition to these 3 losses, one more loss is calculated after merging the output score maps on the 3 scales. These 4 losses are added to calculate the total loss.
+
+# Usage
+### Replicating caffe performance
+To convert the caffemodel released by [authors](https://arxiv.org/abs/1606.00915), download the deeplab-resnet caffemodel (`train_iter_20000.caffemodel`) pretrained on VOC into the data folder. After that, run
+```
+python convert_deeplab_resnet.py
+```
+to generate the corresponding pytorch model file (.pth). The generated .pth snapshot file can be used to get the same test performace as offered by using the caffemodel in caffe.
+If you want to train your model in pytorch, move to the next section.
+### Training 
+Step 1: Convert `init.caffemodel` to a .pth file: `init.caffemodel` contains MS COCO trained weights. We use these weights as initilization for all but the final layer of our model. For the last layer, we use random gaussian with a standard deviation of 0.01 as the initialization.
+To convert `init.caffemodel` to a .pth file, run
+```
+python init_net_surgery.py
+```
+Step 2: Now that we have our initialization, we can train deeplab-resnet by running,
+```
+python train.py
+```
+To get a description of each command-line arguments, run
+```
+python train.py -h
+```
+
+By default, snapshots are saved in every 1000 iterations in the  data/snapshots.
+The following features have been implemented in this repository -
+* Training regime is the same as that of the caffe implementation - SGD with momentum is used, along with the `poly` lr decay policy. A weight decay has been used. The last layer has `10` times the learning rate of other layers.  
+* The iter\_size parameter of caffe has been implemented, effectively increasing the batch\_size to batch\_size times iter\_size
+* Random flipping and random scaling of input has been used as data augmentation.
+* The boundary label (255 in ground truth labels) has not been ignored in the loss function in the current version, instead it has been merged with the background. The ignore\_label caffe parameter would be implemented in the future versions. Post processing using CRF has not been implemented.
+* Batchnorm parameters are kept fixed during training. Also, caffe setting `use_global_stats = True` is reproduced during training. Running mean and variance is not calculated during.
+### Evaluation
+Evaluation of the saved models can be done by running
+```
+python evalpyt.py
+```
+To get a description of each command-line arguments, run
+```
+python evalpyt.py -h
+```
+### Results
+When trained on VOC augmented training set (with 10582 images) using MS COCO pretrained initialization in pytorch, we get a validation performance of 78.49% (mean IOU over all classes, validation set has 1449 images, [authors](https://arxiv.org/abs/1606.00915) report validation performance of 77.69% with their caffe implementation).
+
+To replicate this performance, run 
+```
+train.py --lr 0.00025 --wtDecay 0.0005 --maxIter 20000 --GTpath <train gt images path here> --IMpath <train images path here> --LISTpath data/list/train_aug.txt
+```
+## Acknowledgement
+This work was done during my time at [Video Analytics Lab](http://val.serc.iisc.ernet.in/valweb/). A big thanks to them for their GPUs.
+ 
+A part of the code has been borrowed from [https://github.com/ry/tensorflow-resnet](https://github.com/ry/tensorflow-resnet)
---
+++ pytorch_deeplab_resnet/evalpyt.py
@@ -0,0 +1,129 @@
+import scipy
+from scipy import ndimage
+import cv2
+import numpy as np
+import sys
+#sys.path.insert(0,'/data1/ravikiran/SketchObjPartSegmentation/src/caffe-switch/caffe/python')
+#import caffe
+import torch
+from torch.autograd import Variable
+import torchvision.models as models
+import torch.nn.functional as F
+import deeplab_resnet 
+from collections import OrderedDict
+import os
+from os import walk
+import matplotlib.pyplot as plt
+import torch.nn as nn
+
+from docopt import docopt
+
+docstr = """Evaluate ResNet-DeepLab with 5 branches on sketches of 11 categories (5 super categories)
+
+Usage: 
+    eval_r5.py [options]
+
+Options:
+    -h, --help                  Print this message
+    --visualize                 view outputs of each sketch
+    --LISTpath=<str>            Input image number list file [default: data/list/train_aug.txt]
+    --snapPrefix=<str>          Snapshot [default: VOC12_scenes_]
+    --testGTpath=<str>          Ground truth path prefix [default: data/gt/]
+    --testIMpath=<str>          Sketch images path prefix [default: data/img/]
+    --gpu0=<int>                GPU number [default: 0]
+"""
+
+args = docopt(docstr, version='v0.1')
+print( args)
+
+def get_iou(pred,gt):
+    if pred.shape!= gt.shape:
+        print ('pred shape',pred.shape, 'gt shape', gt.shape)
+    assert(pred.shape == gt.shape)    
+    gt = gt.astype(np.float32)
+    pred = pred.astype(np.float32)
+
+    max_label = 20  # labels from 0,1, ... 20 
+    count = np.zeros((max_label+1,))
+    for j in range(max_label+1):
+        x = np.where(pred==j)
+        p_idx_j = set(zip(x[0].tolist(),x[1].tolist()))
+        x = np.where(gt==j)
+        GT_idx_j = set(zip(x[0].tolist(),x[1].tolist()))
+        #pdb.set_trace()     
+        n_jj = set.intersection(p_idx_j,GT_idx_j)
+        u_jj = set.union(p_idx_j,GT_idx_j)
+    
+        
+        if len(GT_idx_j)!=0:
+            count[j] = float(len(n_jj))/float(len(u_jj))
+
+    result_class = count
+    Aiou = np.sum(result_class[:])/float(len(np.unique(gt))) 
+    
+    return Aiou
+
+def read_file(path_to_file):
+    with open(path_to_file) as f:
+        img_list = []
+        for line in f:
+            img_list.append(line[:-1])
+    return img_list
+
+
+gpu0 = int(args['--gpu0'])
+im_path = args['--testIMpath']
+model = getattr(deeplab_resnet,'Res_Deeplab')()
+model.eval()
+counter = 0
+model.cuda(gpu0)
+snapPrefix = args['--snapPrefix'] 
+gt_path = args['--testGTpath']
+# img_list = open('data/list/val.txt').readlines()
+img_list = read_file(args['--LISTpath'])
+
+for iter in range(1,11):   #TODO set the (different iteration)models that you want to evaluate on. Models are saved during training after each 1000 iters by default.
+    saved_state_dict = torch.load(os.path.join('data/snapshots/',snapPrefix+str(iter)+'000.pth'))
+    # saved_state_dict = torch.load(os.path.join('data/snapshots/', 'MS_DeepLab_resnet_trained_VOC.pth'))
+    if counter==0:
+        print (snapPrefix)
+    counter+=1
+    model.load_state_dict(saved_state_dict)
+
+    pytorch_list = [];
+    for i in img_list:
+        img = np.zeros((513,513,3));
+
+        # img_temp = cv2.imread(os.path.join(im_path,i[:-1]+'.jpg')).astype(float)
+        img_temp = cv2.imread(os.path.join(im_path, i[:-1] + '.tif')).astype(float)
+        img_original = img_temp
+        img_temp[:,:,0] = img_temp[:,:,0] - 104.008
+        img_temp[:,:,1] = img_temp[:,:,1] - 116.669
+        img_temp[:,:,2] = img_temp[:,:,2] - 122.675
+        img[:img_temp.shape[0],:img_temp.shape[1],:] = img_temp
+        # gt = cv2.imread(os.path.join(gt_path,i[:-1]+'.png'),0)
+        png_path = os.path.join(gt_path, i[:-1] + 'segcls.png')
+        png_path = png_path.replace('_8bit','')
+        gt = cv2.imread(png_path, 0)
+        gt[gt==255] = 0
+
+        output = model(Variable(torch.from_numpy(img[np.newaxis, :].transpose(0,3,1,2)).float(),volatile = True).cuda(gpu0))
+        interp = nn.UpsamplingBilinear2d(size=(513, 513))
+        output = interp(output[3]).cpu().data[0].numpy()
+        output = output[:,:img_temp.shape[0],:img_temp.shape[1]]
+        
+        output = output.transpose(1,2,0)
+        output = np.argmax(output,axis = 2)
+        if args['--visualize']:
+            plt.subplot(3, 1, 1)
+            plt.imshow(img_original)
+            plt.subplot(3, 1, 2)
+            plt.imshow(gt)
+            plt.subplot(3, 1, 3)
+            plt.imshow(output)
+            plt.show()
+
+        iou_pytorch = get_iou(output,gt)       
+        pytorch_list.append(iou_pytorch)
+
+    print ('pytorch',iter, np.sum(np.asarray(pytorch_list))/len(pytorch_list))
