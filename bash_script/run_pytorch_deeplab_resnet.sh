931559caf550b18c1565ebaf1e211871b0b8878f
Lingcao Huang <huanglingcao@gmail.com>
2017-05-16 11:07:09 +0800

    typo fix

--- bash_script/run_pytorch_deeplab_resnet.sh
+++ bash_script/run_pytorch_deeplab_resnet.sh
@@ -7,4 +7,4 @@ python /home/lchuang/codes/pytorch-deeplab-resnet/train.py --lr 0.00025 --wtDeca
 
 #python /home/lchuang/codes/pytorch-deeplab-resnet/evalpyt.py --gpu0 7 --testGTpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations  --testIMpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations --snapPrefix spacenet_  --LISTpath test_aug.txt
 
-python /home/lchuang/codes/rsBuildingMapping/pytorch-deeplab-resnet/evalpyt.py --gpu0 7 --testGTpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations  --testIMpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations --snapPrefix spacenet_  --LISTpath test_aug.txt
+python /home/lchuang/codes/rsBuildingMapping/pytorch_deeplab_resnet/evalpyt.py --gpu0 7 --testGTpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations  --testIMpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations --snapPrefix spacenet_  --LISTpath test_aug.txt
