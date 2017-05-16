8117c92120d78850b4beae7b1c10128bfdb908a9
Lingcao Huang <huanglingcao@gmail.com>
2017-05-16 10:45:43 +0800

    update bash

--- bash_script/run_pytorch_deeplab_resnet.sh
+++ bash_script/run_pytorch_deeplab_resnet.sh
@@ -5,4 +5,6 @@
 
 python /home/lchuang/codes/pytorch-deeplab-resnet/train.py --lr 0.00025 --wtDecay 0.0005 --gpu0 6 --maxIter 10000 --GTpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations --IMpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations --LISTpath trainval_aug.txt
 
-python /home/lchuang/codes/pytorch-deeplab-resnet/evalpyt.py --gpu0 7 --testGTpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations  --testIMpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations --snapPrefix spacenet_  --LISTpath test_aug.txt
+#python /home/lchuang/codes/pytorch-deeplab-resnet/evalpyt.py --gpu0 7 --testGTpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations  --testIMpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations --snapPrefix spacenet_  --LISTpath test_aug.txt
+
+python /home/lchuang/codes/rsBuildingMapping/pytorch-deeplab-resnet/evalpyt.py --gpu0 7 --testGTpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations  --testIMpath /home/lchuang/Data/spacenet/voc_format/AOI_4_Shanghai_Train/annotations --snapPrefix spacenet_  --LISTpath test_aug.txt
