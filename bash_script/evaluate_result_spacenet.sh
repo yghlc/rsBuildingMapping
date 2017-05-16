a90f107c727e73d7a9fdc22cdde960bccab8ffb6
Lingcao Huang <huanglingcao@gmail.com>
2017-05-16 18:52:22 +0800

    update evaluate_result_spacenet.sh for EE server

--- bash_script/evaluate_result_spacenet.sh
+++ bash_script/evaluate_result_spacenet.sh
@@ -3,16 +3,16 @@
 #truecsv=$1
 #detectedcsv=$2
 
-spacenet_root=${HOME}/Data/aws_SpaceNet/un_gz
-output_root=${HOME}/Data/aws_SpaceNet/deeplab_exper
+spacenet_root=${HOME}/Data/spacenet
+output_root=${HOME}/experiment/pytorch_deeplab_resnet
 
-exprid=spacenet_rgb_aoi_3
-network=deeplab_largeFOV
+exprid=spacenet_rgb_aoi_4
 
-python_script=${HOME}/codes/PycharmProjects/rsBuildingSeg/SpaceNetChallenge/utilities/python/evaluateScene.py
 
-truecsv=${spacenet_root}/AOI_3_Paris_Train/summaryData/AOI_3_Paris_Train_Building_Solutions.csv
-detectedcsv=${output_root}/${exprid}/features/${network}/val/fc8/result_buildings.csv
+python_script=${HOME}/codes/rsBuildingMapping/SpaceNetChallenge/utilities/python/evaluateScene.py
+
+truecsv=${spacenet_root}/AOI_4_Paris_Train/summaryData/AOI_4_Paris_Train_Building_Solutions.csv
+detectedcsv=${output_root}/${exprid}/results/result_buildings.csv
 
 echo SpaceNetTruthFile: ${truecsv}
 echo SpaceNetProposalFile: ${detectedcsv}
