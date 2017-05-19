910e5745a71f9e5d424ea936eec45cd92fdc5d1e
Lingcao Huang <huanglingcao@gmail.com>
2017-05-19 21:17:00 +0800

    bug fix

--- bash_script/fine_tune_all.sh
+++ bash_script/fine_tune_all.sh
@@ -5,6 +5,8 @@ AOI_3=spacenet_rgb_aoi_3_test
 AOI_4=spacenet_rgb_aoi_4_test
 AOI_5=spacenet_rgb_aoi_5_test
 
+mkdir result_csv
+
 for AOI in ${AOI_2}  ${AOI_3} ${AOI_4} ${AOI_5}
 do
         ./fine_tune_city_cmd.sh ${AOI}
