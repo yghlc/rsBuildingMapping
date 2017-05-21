236263b8d2800f61b40f0bd6b13a71f1d35cd5ca
Lingcao Huang <huanglingcao@gmail.com>
2017-05-21 14:38:25 +0800

    remove edge detection

--- bash_script/fine_tune_city.sh
+++ bash_script/fine_tune_city.sh
@@ -15,28 +15,32 @@ cp ${dir}/spacenet_rgb_aoi_2-4/list/replace_wrong_path.py .
 
 cat train_aug.txt test_aug.txt > train_aug_all.txt
 cp train_aug_all.txt train_aug.txt
-#python replace_wrong_path.py train_aug.txt
-#python replace_wrong_path.py val.txt
+python replace_wrong_path.py train_aug.txt
+python replace_wrong_path.py val.txt
 cd ..
 
 #prepare model
 mkdir model
 mkdir model/deeplab_largeFOV
-cp ${dir}/spacenet_rgb_aoi_2-4/model/deeplab_largeFOV/train_iter_6000.caffemodel model/deeplab_largeFOV/init.caffemodel
+cp ${dir}/spacenet_rgb_aoi_2-4/model/deeplab_largeFOV/train_iter_48000.caffemodel model/deeplab_largeFOV/init.caffemodel
 
 #prepare config
 cp -r ~/experiment/caffe_deeplab/spacenet_rgb_aoi_2-4/config .
+
+cd ~/codes/rsBuildingSeg
+git pull
+cd -
 
 # run fine train with deeplab
 cp ~/codes/rsBuildingSeg/DeepLab-Context/run_train.py .
 python run_train.py ${dir}/${city}  ${gpuid}
 
 # produce the edge map of Test public data
-mkdir edge
-cd edge
-cp ~/codes/rcf/examples/rcf_building_edge/edge.sh .
-./edge.sh ../list/val.txt ${gpuid}
-cd ..
+#mkdir edge
+#cd edge
+#cp ~/codes/rcf/examples/rcf_building_edge/edge.sh .
+#./edge.sh ../list/val.txt ${gpuid}
+#cd ..
 
 # run test on Test public data
 cp ~/codes/rsBuildingSeg/DeepLab-Context/run_test_and_evaluate.py .
