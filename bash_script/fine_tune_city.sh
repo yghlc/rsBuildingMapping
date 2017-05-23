371729602bdc6c251c0909c0ce65eeefec80a70b
Lingcao Huang <huanglingcao@gmail.com>
2017-05-23 10:42:23 +0800

    bug fix

--- bash_script/fine_tune_city.sh
+++ bash_script/fine_tune_city.sh
@@ -23,7 +23,7 @@ if [ "$bresuming" = false ] ; then
 python ${deeplab}/run_train.py ${dir}/${city}  ${gpuid}
 else
 # get latest solverstate file
-newest_solverstate=$(ls -t ${dir}/${city}/model/deeplab_largeFOV/*.caffemodel | head -1)
+newest_solverstate=$(ls -t ${dir}/${city}/model/deeplab_largeFOV/*.solverstate | head -1)
 echo "resuming with:" ${newest_solverstate}
 ${deeplab}/.build_release/tools/caffe.bin  train --solver=${dir}/${city}/config/${net}/solver_train_aug.prototxt --snapshot=${newest_solverstate}
 fi
